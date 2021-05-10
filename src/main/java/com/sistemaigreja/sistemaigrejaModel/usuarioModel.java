e174386bd4d1bd784d19e2b17688683699d239ca
Kauan Manoel <kauanmanoel4@gmail.com>
2021-05-10 01:42:38 -0300

    version 1.1

--- src/main/java/com/sistemaigreja/sistemaigrejaModel/usuarioModel.java
+++ src/main/java/com/sistemaigreja/sistemaigrejaModel/usuarioModel.java
@@ -1,5 +1,66 @@
 package com.sistemaigreja.sistemaigrejaModel;
 
-public class usuarioModel {
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
 
+@Entity
+@Table(name = "usuario")
+
+
+public class usuarioModel {
+	
+	private long id;
+	
+	private String nome_usuario;
+	
+	private String departamento;
+	
+	private String email;
+	
+	private String senha;
+
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome_usuario() {
+		return nome_usuario;
+	}
+
+	public void setNome_usuario(String nome_usuario) {
+		this.nome_usuario = nome_usuario;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+	
 }
